299566aba7daaf51adbb0f98e507594dc222a087
ahmeddin12 <amudi621@gmail.com>
2025-09-26 23:27:36 +0300

    Create tracking component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { Checkout } from "./pages/Checkout";
 import { Orders } from "./pages/Orders";
+import { Tracking } from "./pages/Tracking";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="/tracking" element={<Tracking />} />
       </Routes>
     </>
   );
