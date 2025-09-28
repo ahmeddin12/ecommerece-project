c4a4a1d6564ab979c9b9a1d272414de7f81df8ac
ahmeddin12 <amudi621@gmail.com>
2025-09-28 11:53:51 +0300

    fetch data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,14 @@
 import { Header } from "../components/Header";
 import { Products } from "../components/Products";
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
