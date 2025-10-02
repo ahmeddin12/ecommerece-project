522007198cbb158a6e4584f24e2a6222ef2b5b83
ahmeddin12 <amudi621@gmail.com>
2025-10-02 15:26:14 +0300

    creare add to cart feature

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,13 +1,13 @@
 import { Header } from "../../components/Header";
 import { Products } from "../../components/Products";
 
-export function HomePage({ carts }) {
+export function HomePage({ carts, onAddToCart }) {
   return (
     <>
       <title>Ecommerce Project</title>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header carts={carts} />
-      <Products />
+      <Products onAddToCart={onAddToCart} />
       <div className="home-page"></div>
     </>
   );
